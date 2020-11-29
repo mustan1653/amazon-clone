901697fd52f333ec99d17d171e44e1cfbdc5d465
mustan1653 <mustan.rampurawala@gmail.com>
2020-11-29 11:01:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHKQVTq5mXlcFfvBPjAhqgUPgsnN-Yn20",
+    apiKey: "",
     authDomain: "clone-ff4d5.firebaseapp.com",
     databaseURL: "https://clone-ff4d5.firebaseio.com",
     projectId: "clone-ff4d5",
     storageBucket: "clone-ff4d5.appspot.com",
     messagingSenderId: "349861636451",
-    appId: "1:349861636451:web:219956773fd3c8aaaf8b56",
+    appId: "",
     measurementId: "G-F0F30Z4FKF"
   };
 
